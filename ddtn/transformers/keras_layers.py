137b124f7dc7b1d2beeef1642916d92612dd272e
Nicki Skafte <skaftenicki@gmail.com>
2018-05-16 16:31:48 +0200

    small cleaning of files

--- ddtn/transformers/keras_layers.py
+++ ddtn/transformers/keras_layers.py
@@ -7,13 +7,18 @@ Created on Mon May 14 14:35:01 2018
 
 #%%
 from keras.layers.core import Layer
+from ddtn.transformers.transformer_layers import ST_Affine_transformer
 
 #%%
-class SpatialAffineTransformer(Layer):
+class BaseTransformerLayer(Layer):
+    """ Base class for defining the transformers as keras layers. Since the 
+        __init__(...), compute_output_shape(...) and build(...) methods are the 
+        same for all layers, we define these in this base layer
+    """
     def __init__(self, localization_net, output_size):
         self.locnet = localization_net
         self.output_size = output_size
-        super(SpatialAffineTransformer, self).__init__()
+        super(BaseTransformerLayer, self).__init__()
     
     def compute_output_shape(self, input_shape):
         output_size = self.output_size
@@ -23,8 +28,12 @@ class SpatialAffineTransformer(Layer):
         self.locnet.build(input_shape)
         self.trainable_weights = self.locnet.trainable_weights
         self.constraints = self.locnet.constraints
-        
+    
+    def call(self, X, mask=None):
+        raise NotImplementedError("Must override call method")
+#%%
+class SpatialAffineLayer(BaseTransformerLayer):
     def call(self, X, mask=None):
         theta = self.locnet.call(X)
-        output = ST_Affine_transformer(theta, X, self.output_size)
+        output = ST_Affine_transformer(X, theta, self.output_size)
         return output
